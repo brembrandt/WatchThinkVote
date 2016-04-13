b57d86640a8c0a3820e068f0229399b2caae0a2a
brembrandt <me@brianrembrandt.com>
2016-04-12 22:32:20 -0600

    Added files via upload

---
+++ wor/wo_10151/justabutton_edge.js
@@ -0,0 +1,7 @@
+
+(function(compId){var _=null,y=true,n=false,x4='rgba(192,192,192,1)',e5='body > button:nth-child(1)',x3='6.0.0.400',x2='5.0.0',w='width',m='rect',x1='6.0.0',i='none';var im='images/',aud='media/',vid='media/',js='js/',fonts={},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[],symbols={"stage":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:'Rectangle3',t:m,r:['172px','258px','76px','24px','auto','auto'],f:[x4],s:[0,"rgb(0, 0, 0)",i]}],style:{}},tt:{d:0,a:y,data:[["eid1",w,0,0,"linear",e5,'203px','203px']]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-237389221");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+//Edge symbol: 'stage'
+(function(symbolName){})("stage");
+//Edge symbol end:'stage'
+})})(AdobeEdge.$,AdobeEdge,"EDGE-237389221");
